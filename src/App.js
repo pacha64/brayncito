c00a9bb4b4af641eac43b4cc1683bf46a21031b1
Fernando Leboran <f.leboran@gmail.com>
2022-10-07 13:53:14 -0300

    bryan penes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
+  const [count, setCount] = React.useState(1);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <img src={"https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fs1.pictoa.com%2Fmedia%2Fgalleries%2F172%2F700%2F172700594c19c2e7b86%2F3264837594c19c3056c4.jpg&f=1&nofb=1&ipt=ed6c6c1a8beafe6c3698901a6ff451ab766ba45d6eaedee64dd11e6d71b96481&ipo=images"} className="App-logo" alt="logo" />
+    <p>
+      Bryancito tiene {count} penes en la boca
+    </p>
+    <button
+      onClick={() => {
+        setCount(count + 1);
+      }}
+    >Quiere mas</button>
     </div>
   );
 }
